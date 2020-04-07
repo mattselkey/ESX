849db715a338d45286c108a3349d3b2404cff90c
mattselkey <matt@mattselkey.com>
2020-04-07 12:24:53 +0200

    new esx stuff for lab shutdown

--- Invoke-VMLabShutdown.ps1
+++ Invoke-VMLabShutdown.ps1
@@ -26,4 +26,47 @@ param (
     [String]
     $Pass
 )
+BEGIN{
+    try {
+        $InformationPreference = "Continue"
+        #$cred = Get-Credential 
+        Write-Information -Message "Connecting to host"
+        set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false
+        connect-viserver –server $ESXHost -User $User -Password $Pass 
+        #connect-viserver –server $ESXHost -Credential $cred
+    }
+    catch {
+        Write-Information -Message "error connecting to host, error is ($_)"
+    }
+     
+    }
+
+    PROCESS{
+        Write-Information -Message "Getting Power Managed VMs"
+        try{
+        $VMs = Get-VM | Where-Object {Get-TagAssignment $_ | Where-Object{ $_.Tag -like "*POWERMANAGE*"}}
+        }catch{
+       
+        }
+       
+       foreach ($VM in $VMs) {
+         
+        if($VM.powerstate -eq "PoweredOn"){
+           
+            Write-Information -Message "Suspending VM ($VM)"
+            if (Get-TagAssignment VM -like "*SUSPEND*") {
+                Suspend-VM $VM 
+            }
+            else{
+                Write-Information -Message "Shutting down VM ($VM)"
+                Stop-VMGuest  $VM -Confirm:$false
+
+            }
+         }   
+       } 
+    }
+       
+       END{
+       Disconnect-VIServer  -Confirm:$false
+       }
 
